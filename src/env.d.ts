9aed5a569a28991c49e5dbb545911ef115e11d74
Aklilu Beyero <141402232+aka8338@users.noreply.github.com>
2025-07-16 23:19:33 +0300

    fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_FRONT_END: string;
+  readonly;
+
+  VITE_FRONT_END: string;
   // add more keys here as you add more VITE_ variables
 }
 
